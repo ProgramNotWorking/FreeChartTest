4f22b725aed34c053b8520ecd22d3c727b0c4282
ProgramNotWorking <106459487+ProgramNotWorking@users.noreply.github.com>
2024-04-07 20:20:24 +0300

    Some tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,17 +7,16 @@ import org.jfree.data.xy.XYSeriesCollection
 import javax.swing.JFrame
 import kotlin.math.sin
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val dataset = XYSeriesCollection()
     val wSeries = XYSeries("w(t)")
     val bSeries = XYSeries("b")
     val aSeries = XYSeries("a")
     val xSeries = XYSeries("x(t)")
+    val mainSeries = XYSeries("t")
 
     val a = -1.0
-    val l = 1.0
+    val l = 2.0
     val b = 2.996
     val w0 = 0.0
     val K = 1000000.0
@@ -27,9 +26,14 @@ fun main() {
     fun x(t: Double) = 3 * sin(t)
 
     for (t in 0..(T * 10).toInt() step 1) {
-        val w = w0 // w(0) solving method here
+        val w = if (x((t / 10).toDouble()) >= 0) {
+            1.0
+        } else {
+            0.0
+        }
+        mainSeries.add((t / 10).toDouble(), 0)
         wSeries.add((t / 10).toDouble(), w)
-        bSeries.add((t / 10).toDouble(), b)
+        bSeries.add((t / 10).toDouble(), l) // may be 'b' value
         aSeries.add((t / 10).toDouble(), a)
         xSeries.add((t / 10).toDouble(), x((t / 10).toDouble()))
     }
@@ -38,6 +42,7 @@ fun main() {
     dataset.addSeries(bSeries)
     dataset.addSeries(aSeries)
     dataset.addSeries(xSeries)
+    dataset.addSeries(mainSeries)
 
     val chart: JFreeChart = ChartFactory.createXYLineChart(
         "График",
